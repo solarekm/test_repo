b42267598620d02795b60e6d7f001848b71fb27a
Michal Solarek <michal.solarek@partner.bmwgroup.com>
2023-11-14 13:32:20 +0100

    test 234

--- update_wiki.py
+++ update_wiki.py
@@ -1,20 +1,24 @@
 import os
 
 user_id = os.environ['USER_ID']
-current_date = os.environ.get('CURRENT_DATE')  # Zmieniono na os.environ.get dla lepszej obsługi błędów
+current_date = os.environ.get('CURRENT_DATE')  # Użyj os.environ.get dla bezpieczeństwa
 numer_id = os.environ['NUMER_ID']
-github_actor = os.environ['GITHUB_ACTOR']  # Użytkownik, który uruchomił workflow
+github_actor = os.environ['GITHUB_ACTOR']
 
 wiki_file = 'test_repo.wiki/wiki-update.md'
 
-# Tworzenie nowego pliku, jeśli nie istnieje
-if not os.path.exists(wiki_file):
-    with open(wiki_file, 'w') as file:
-        file.write("## Workflow Log\n\n")
-        file.write("| Date | GitHub Actor | Numer ID | User ID |\n")
-        file.write("| --- | --- | --- | --- |\n")
+new_entry = f"| {user_id} | {current_date} | {numer_id} | {github_actor} |\n"
 
-# Dodawanie nowego wpisu
-with open(wiki_file, 'a') as file:
-    file.write(f"| {current_date} | {github_actor} | {numer_id} | {user_id} |\n")
+# Sprawdzanie istnienia pliku i odczytywanie zawartości
+if os.path.exists(wiki_file):
+    with open(wiki_file, 'r') as file:
+        content = file.readlines()
+else:
+    content = ["## Workflow Log\n\n", "| User ID | Date | Numer ID | GitHub Actor |\n", "| --- | --- | --- | --- |\n"]
 
+# Dodawanie nowego wpisu na początku tabeli (po nagłówkach)
+content.insert(3, new_entry)
+
+# Zapisywanie zaktualizowanej zawartości do pliku
+with open(wiki_file, 'w') as file:
+    file.writelines(content)
