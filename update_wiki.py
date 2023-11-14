68cfb2a0191d57ed97b99362bfd9fa9a051c5582
Michal Solarek <michal.solarek@partner.bmwgroup.com>
2023-11-14 13:09:40 +0100

    2 test

--- update_wiki.py
+++ update_wiki.py
@@ -1,23 +1,18 @@
 import os
 
-# Pobierz zmienne środowiskowe
 user_id = os.environ['USER_ID']
 current_date = os.environ['CURRENT_DATE']
-numer_id = os.environ['NUMER_ID']  # Nowa zmienna środowiskowa
+numer_id = os.environ['NUMER_ID']
 
-wiki_file = 'wiki-update.md'
-new_entry = f"| {user_id} | {current_date} | {numer_id} |\n"
+wiki_file = 'test_repo.wiki/wiki-update.md'
 
-# Twórz lub aktualizuj plik Wiki
+# Tworzenie nowego pliku, jeśli nie istnieje
 if not os.path.exists(wiki_file):
     with open(wiki_file, 'w') as file:
         file.write("## Workflow Log\n\n")
-        file.write("| User ID | Date | Numer ID |\n")  # Nagłówek tabeli z trzema kolumnami
+        file.write("| User ID | Date | Numer ID |\n")
         file.write("| --- | --- | --- |\n")
-        file.write(new_entry)
-else:
-    with open(wiki_file, 'r+') as file:
-        content = file.readlines()
-        content.insert(3, new_entry)  # Wstaw nowy wpis pod nagłówkami
-        file.seek(0)
-        file.writelines(content)
+
+# Dodawanie nowego wpisu
+with open(wiki_file, 'a') as file:
+    file.write(f"| {user_id} | {current_date} | {numer_id} |\n")
